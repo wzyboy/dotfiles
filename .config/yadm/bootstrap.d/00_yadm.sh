15e72a8f8dbf02e5a153a9065dac74730e4435d6
Zhuoyun Wei <wzyboy@wzyboy.org>
2023-06-25 12:29:46 -0700

    feat: init submodules on bootstrap

--- .config/yadm/bootstrap.d/00_yadm.sh
+++ .config/yadm/bootstrap.d/00_yadm.sh
@@ -1,6 +1,6 @@
 #!/bin/bash -
 
+# Hide non-dotfiles in the repo root
 # https://github.com/TheLocehiliosan/yadm/issues/93#issuecomment-886667802
-
 yadm gitconfig core.sparseCheckout true
 yadm sparse-checkout set '/*' '!README.md'
