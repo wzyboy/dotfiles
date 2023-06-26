d6bdcdcbcd686a34294b3dd739ec843111e0e236
Zhuoyun Wei <wzyboy@wzyboy.org>
2023-06-25 22:16:02 -0700

    fixup! feat: bootstrap nvim

--- .config/yadm/bootstrap.d/10_nvim.sh
+++ .config/yadm/bootstrap.d/10_nvim.sh
@@ -3,4 +3,4 @@
 curl --create-dirs -Lo ~/.local/share/nvim/site/autoload/plug.vim \
     https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
 
-nvim '+PlugInstall' '+qall'
+nvim '+PlugInstall' '+qa!'
