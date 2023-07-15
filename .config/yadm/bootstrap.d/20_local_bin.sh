d6977be430622770288a639a4be4e944278137c1
Zhuoyun Wei <wzyboy@wzyboy.org>
2023-07-14 22:20:31 -0700

    feat: add git-fire

--- .config/yadm/bootstrap.d/20_local_bin.sh
+++ .config/yadm/bootstrap.d/20_local_bin.sh
@@ -3,8 +3,9 @@
 mkdir -p ~/.local/bin
 pushd ~/.local/bin
 
-# diff-so-fancy
+# binaries installed via git
 ln -svf ../diff-so-fancy/diff-so-fancy
+ln -svf ../git-fire/git-fire
 
 # fzf
 ../fzf/install --all
