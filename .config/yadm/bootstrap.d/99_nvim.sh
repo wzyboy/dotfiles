ce876f0c6c0e038968f4d9fbeb7640a41749d34d
Zhuoyun Wei <wzyboy@wzyboy.org>
2024-06-10 18:14:21 -0700

    feat(nvim): bootstrap in Git for Windows

--- .config/yadm/bootstrap.d/99_nvim.sh
+++ .config/yadm/bootstrap.d/99_nvim.sh
@@ -1,6 +1,10 @@
 #!/bin/bash -
 
-plug_path=~/.local/share/nvim/site/autoload/plug.vim
+if [[ "$MSYSTEM" == "MINGW64" ]]; then
+  plug_path=~/AppData/Local/nvim-data/site/autoload/plug.vim
+else
+  plug_path=~/.local/share/nvim/site/autoload/plug.vim
+fi
 
 [[ -f $plug_path ]] && exit 0
 
