b9d71dbd52110338b6cff938a9bf61ef0620a708
Zhuoyun Wei <wzyboy@wzyboy.org>
2024-06-13 12:08:51 -0700

    fix(nvim): re-enable Noevide animation

--- .config/nvim/lua/init_neovide.lua
+++ .config/nvim/lua/init_neovide.lua
@@ -1,5 +1,5 @@
 -- https://neovide.dev/configuration.html
 if vim.g.neovide then
     vim.o.guifont = "Iosevka Term:h12"
-    vim.g.neovide_cursor_animation_length = 0
+    --vim.g.neovide_cursor_animation_length = 0
 end
