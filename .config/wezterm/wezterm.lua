335e27ef1d83a534dd1016dcf320f5a128399487
Zhuoyun Wei <wzyboy@wzyboy.org>
2025-08-30 01:15:21 -0700

    wezterm: git for windows config

--- .config/wezterm/wezterm.lua
+++ .config/wezterm/wezterm.lua
@@ -57,32 +57,12 @@ local config = {
 }
 
 if wezterm.target_triple == "x86_64-pc-windows-msvc" then
-    if string.match(wezterm.hostname(), '^C') then
-        config.prefer_egl = true
-    end
-    -- config.front_end = "Software" -- OpenGL doesn't work quite well with RDP.
-    -- config.term = "" -- Set to empty so FZF works on windows
-    table.insert(config.launch_menu, { label = "PowerShell", args = {"powershell.exe", "-NoLogo"} })
-
-    -- Find installed visual studio version(s) and add their compilation
-    -- environment command prompts to the menu
-    for _, vsvers in ipairs(wezterm.glob("Microsoft Visual Studio/20*", "C:/Program Files (x86)")) do
-        local year = vsvers:gsub("Microsoft Visual Studio/", "")
-        table.insert(config.launch_menu, {
-            label = "x64 Native Tools VS " .. year,
-            args = {"cmd.exe", "/k", "C:/Program Files (x86)/" .. vsvers .. "/BuildTools/VC/Auxiliary/Build/vcvars64.bat"},
-        })
-    end
+    local git_bash = "C:/Program Files/Git/bin/bash.exe"
+    config.default_prog = { git_bash, "-i", "-l" }
+    table.insert(config.launch_menu, { label = "Git Bash", args = { git_bash, "-i", "-l" } })
 else
-    local fish_bin_path = "/bin/fish"
-    if file_exists("/opt/homebrew/bin/fish") then
-        fish_bin_path = "/opt/homebrew/bin/fish"
-        config.default_prog = { '/opt/homebrew/bin/fish', '-l' }
-    else
-        config.default_prog = { '/bin/bash', '-l' }
-    end
-    table.insert(config.launch_menu, { label = "fish", args = {fish_bin_path, "-l"} })
-    table.insert(config.launch_menu, { label = "bash", args = {"bash", "-l"} })
+    config.default_prog = { '/bin/bash', '-l' }
+    table.insert(config.launch_menu, { label = "bash", args = {"bash", "-i"} })
 end
 
 return config
