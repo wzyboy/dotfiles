fed98d6bd424b7d02b0611e855edfff1efd06615
Zhuoyun Wei <wzyboy@wzyboy.org>
2025-08-30 01:46:38 -0700

    wezterm: make it more like tmux

--- .config/wezterm/wezterm.lua
+++ .config/wezterm/wezterm.lua
@@ -14,18 +14,20 @@ local config = {
         saturation = 1.0,
         brightness = 1.0,
     },
+    initial_cols = 120,
+    initial_rows = 30,
+    tab_bar_at_bottom = true,
+    use_fancy_tab_bar = false,
     font = wezterm.font 'Iosevka Term',
     font_size = 12.0,
     launch_menu = {},
-    leader = { key="b", mods="CTRL" },
+    leader = { key = "b", mods = "CTRL" },
     disable_default_key_bindings = true,
     keys = {
-        -- Send "CTRL-B" to the terminal when pressing CTRL-B, CTRL-B
-        -- { key = "b", mods = "LEADER|CTRL",  action=wezterm.action{SendString="\x01"}},
         { key = "-", mods = "LEADER",       action=wezterm.action{SplitVertical={domain="CurrentPaneDomain"}}},
         { key = "|", mods = "LEADER|SHIFT", action=wezterm.action{SplitHorizontal={domain="CurrentPaneDomain"}}},
         { key = "z", mods = "LEADER",       action="TogglePaneZoomState" },
-        { key = "c", mods = "LEADER",       action=wezterm.action{SpawnTab="CurrentPaneDomain"}},
+        { key = "c", mods = "LEADER",       action=wezterm.action{SpawnCommandInNewTab={cwd=wezterm.home_dir}}},
         { key = "h", mods = "LEADER",       action=wezterm.action{ActivatePaneDirection="Left"}},
         { key = "j", mods = "LEADER",       action=wezterm.action{ActivatePaneDirection="Down"}},
         { key = "k", mods = "LEADER",       action=wezterm.action{ActivatePaneDirection="Up"}},
@@ -43,25 +45,27 @@ local config = {
         { key = "7", mods = "LEADER",       action=wezterm.action{ActivateTab=6}},
         { key = "8", mods = "LEADER",       action=wezterm.action{ActivateTab=7}},
         { key = "9", mods = "LEADER",       action=wezterm.action{ActivateTab=8}},
+        { key = "n", mods = "LEADER",       action=wezterm.action{ActivateTabRelative=1}},
+        { key = "p", mods = "LEADER",       action=wezterm.action{ActivateTabRelative=-1}},
         { key = "&", mods = "LEADER|SHIFT", action=wezterm.action{CloseCurrentTab={confirm=true}}},
         { key = "x", mods = "LEADER",       action=wezterm.action{CloseCurrentPane={confirm=true}}},
 
-        { key = "n", mods="SHIFT|CTRL",     action="ToggleFullScreen" },
-        { key = "v", mods="SHIFT|CTRL",     action=wezterm.action.PasteFrom 'Clipboard'},
-        { key = "c", mods="SHIFT|CTRL",     action=wezterm.action.CopyTo 'Clipboard'},
-        { key = "+", mods="SHIFT|CTRL",     action="IncreaseFontSize" },
-        { key = "-", mods="SHIFT|CTRL",     action="DecreaseFontSize" },
-        { key = "0", mods="SHIFT|CTRL",     action="ResetFontSize" },
+        { key = "n", mods = "SHIFT|CTRL",   action="ToggleFullScreen" },
+        { key = "v", mods = "SHIFT|CTRL",   action=wezterm.action.PasteFrom 'Clipboard'},
+        { key = "c", mods = "SHIFT|CTRL",   action=wezterm.action.CopyTo 'Clipboard'},
+        { key = "+", mods = "SHIFT|CTRL",   action="IncreaseFontSize" },
+        { key = "-", mods = "SHIFT|CTRL",   action="DecreaseFontSize" },
+        { key = "0", mods = "SHIFT|CTRL",   action="ResetFontSize" },
     },
     set_environment_variables = {},
 }
 
 if wezterm.target_triple == "x86_64-pc-windows-msvc" then
     local git_bash = "C:/Program Files/Git/bin/bash.exe"
-    config.default_prog = { git_bash, "-i", "-l" }
-    table.insert(config.launch_menu, { label = "Git Bash", args = { git_bash, "-i", "-l" } })
+    config.default_prog = { git_bash, "-i" }
+    table.insert(config.launch_menu, { label = "Git Bash", args = { git_bash, "-i" } })
 else
-    config.default_prog = { '/bin/bash', '-l' }
+    config.default_prog = { "/bin/bash", "-i" }
     table.insert(config.launch_menu, { label = "bash", args = {"bash", "-i"} })
 end
 
