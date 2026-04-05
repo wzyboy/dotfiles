c51f83f8ad6394e2514173b9acc46bd5722b2706
Zhuoyun Wei <wzyboy@wzyboy.org>
2026-04-05 02:43:46 -0700

    nvim: (neovide) use Sarasa on windows

--- .config/nvim/lua/init_neovide.lua
+++ .config/nvim/lua/init_neovide.lua
@@ -1,6 +1,10 @@
 -- https://neovide.dev/configuration.html
 if vim.g.neovide then
-    vim.o.guifont = "Iosevka Term:h12"
+    if vim.fn.has('win32') == 1 then
+        vim.o.guifont = 'Sarasa Term CL:h12'
+    else
+        vim.o.guifont = 'Iosevka Term:h12'
+    end
     --vim.g.neovide_cursor_animation_length = 0
     vim.opt.clipboard = "unnamedplus"
 end
