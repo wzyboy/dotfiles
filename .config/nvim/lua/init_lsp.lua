5819bb7aad3df557fd2d81d8443aa57de581cad3
Zhuoyun Wei <wzyboy@wzyboy.org>
2026-04-23 15:43:10 -0700

    Revert "nvim: enabel lsp semantics token highlight"
    This reverts commit 1ca2b772d64f769f85bd021249901a2801c93bca.

--- .config/nvim/lua/init_lsp.lua
+++ .config/nvim/lua/init_lsp.lua
@@ -9,9 +9,9 @@ vim.diagnostic.config({
 
 -- Hide LSP semantic highlights (use treesitter)
 -- https://neovim.io/doc/user/lsp.html#lsp-semantic-highlight
--- for _, group in ipairs(vim.fn.getcompletion("@lsp", "highlight")) do
---   vim.api.nvim_set_hl(0, group, {})
--- end
+for _, group in ipairs(vim.fn.getcompletion("@lsp", "highlight")) do
+  vim.api.nvim_set_hl(0, group, {})
+end
 
 -- lsp_signature
 require('lsp_signature').setup({
